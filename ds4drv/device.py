4b12688b0013011685b75242a9ca17f47a27da81
kamibukuro5656 <amacoa913@gmail.com>
2025-03-27 16:22:39 +0900

    delete dualsense control

--- ds4drv/device.py
+++ ds4drv/device.py
@@ -1,6 +1,5 @@
 from struct import Struct
 from sys import version_info as sys_version
-from binascii import crc32
 
 class StructHack(Struct):
     """Python <2.7.4 doesn't support struct unpack from bytearray."""
@@ -286,46 +285,7 @@ class DSenseDevice(object):
     def control(self, big_rumble=0, small_rumble=0,
                 led_red=0, led_green=0, led_blue=0,
                 flash_led1=0, flash_led2=0):
-        if self.type == "bluetooth":
-            pkt = bytearray(79)
-            pkt[:5] = [0xa2, 0x11, 0x80, 0x00, 0xff]
-            offset = 4
-            report_id = 0x11
-            send_from = 2
-
-        elif self.type == "usb":
-            pkt = bytearray(31)
-            pkt[0] = 255
-            offset = 0
-            report_id = 0x05
-            send_from = 0
-
-        else:
-            raise ValueError("Type is neither bluetooth nor usb")
-
-        # Rumble
-        pkt[offset+3] = min(small_rumble, 255)
-        pkt[offset+4] = min(big_rumble, 255)
-
-        # LED (red, green, blue)
-        pkt[offset+5] = min(led_red, 255)
-        pkt[offset+6] = min(led_green, 255)
-        pkt[offset+7] = min(led_blue, 255)
-
-        # Time to flash bright (255 = 2.5 seconds)
-        pkt[offset+8] = min(flash_led1, 255)
-
-        # Time to flash dark (255 = 2.5 seconds)
-        pkt[offset+9] = min(flash_led2, 255)
-
-        # Last 4 bytes are CRC32
-        crc = crc32(pkt[:-4])
-        pkt[-4] = (crc & 0x000000ff)
-        pkt[-3] = (crc & 0x0000ff00) >> 8
-        pkt[-2] = (crc & 0x00ff0000) >> 16
-        pkt[-1] = (crc & 0xff000000) >> 24
-
-        # self.write_report(report_id, pkt[send_from:])
+        pass # not implemented
 
     def parse_report(self, buf):
         """Parse a buffer containing a HID report."""
